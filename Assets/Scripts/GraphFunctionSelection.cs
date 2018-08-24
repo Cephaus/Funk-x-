54585c70b500ff42b05d00ca205050d8e3865ab9
Cephaus <yfndws@gmail.com>
2018-08-24 10:19:04 -0400

    initial commit

---
+++ Assets/Scripts/GraphFunctionSelection.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class GraphFunctionSelection : MonoBehaviour {
+
+    Graph3DCustom graphscript;
+
+    void Start()
+    {
+        graphscript = GetComponent<Graph3DCustom>();
+    }
+
+    public void Sine()
+    {
+        graphscript.function = Graph3DCustom.GraphFunctionName.Sine;
+    }
+
+    public void Sine2D()
+    {
+        graphscript.function = Graph3DCustom.GraphFunctionName.Sine2D;
+    }
+
+    public void MultiSine()
+    {
+        graphscript.function = Graph3DCustom.GraphFunctionName.MultiSine;
+    }
+
+    public void MultiSine2D()
+    {
+        graphscript.function = Graph3DCustom.GraphFunctionName.MultiSine2D;
+    }
+
+    public void Ripple()
+    {
+        graphscript.function = Graph3DCustom.GraphFunctionName.Ripple;
+    }
+
+    public void Cylinder()
+    {
+        graphscript.function = Graph3DCustom.GraphFunctionName.Cylinder;
+    }
+
+    public void Sphere()
+    {
+        graphscript.function = Graph3DCustom.GraphFunctionName.Sphere;
+    }
+
+    public void Torus()
+    {
+        graphscript.function = Graph3DCustom.GraphFunctionName.Torus;
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKey("escape"))
+            Application.Quit();
+
+    }
+}
